c0bca43dd38fe2c2dc745abceae000c9f2aa9b29
Joel Lowery <joeltlowery@gmail.com>
2016-10-09 00:46:58 -0500

    Added mean squared error

--- machine_learning_implementations/metrics/general.py
+++ machine_learning_implementations/metrics/general.py
@@ -11,3 +11,12 @@ def mean_absolute_error(predictions: typing.Iterable[AnyNum],
         sum += abs(prediction - actual)
         count += 1
     return sum / count
+
+def mean_squared_error(predictions: typing.Iterable[AnyNum],
+                        actuals: typing.Iterable[AnyNum]) -> float:
+    sum = 0
+    count = 0
+    for prediction, actual in zip(predictions, actuals):
+        sum += (prediction - actual)**2
+        count += 1
+    return sum / count
--- machine_learning_implementations/tests/test_metrics.py
+++ machine_learning_implementations/tests/test_metrics.py
@@ -1,4 +1,3 @@
-import pytest
 import machine_learning_implementations.metrics.general as metrics
 
 
@@ -13,3 +12,20 @@ class TestMeanAbsoluteError:
         actuals = [1.0, 1.0, 0.0]
         preds = [1.0, 1.0, 1.0]
         assert abs(metrics.mean_absolute_error(actuals, preds) - 0.33333) < 0.00001
+
+class TestMeanSquaredError:
+
+    def test_zero_case(self):
+        actuals = [1.0, 1.0, 0.0]
+        preds = [1.0, 1.0, 0.0]
+        assert metrics.mean_squared_error(actuals, preds) == 0.0
+
+    def test_nonzero_case(self):
+        actuals = [1.0, 1.0, 0.0]
+        preds = [1.0, 1.0, 1.0]
+        assert abs(metrics.mean_squared_error(actuals, preds) - 0.33333) < 0.00001
+
+    def test_single_element_case(self):
+        actuals = [2.0]
+        preds = [5.0]
+        assert metrics.mean_squared_error(actuals, preds) == 9.0
