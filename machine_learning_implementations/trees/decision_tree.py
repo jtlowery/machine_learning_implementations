8df6f00fc9e3329639e4898cdfe9c842efa2b3ba
Joel Lowery <joeltlowery@gmail.com>
2016-12-01 23:34:51 -0600

    Partial work on trees

--- machine_learning_implementations/trees/decision_tree.py
+++ machine_learning_implementations/trees/decision_tree.py
@@ -6,7 +6,7 @@ class Leaf:
     def __init__(self, label: int):
         self.label = label
 
-    def predict(self, data: typing.Iterable):
+    def predict(self, data: typing.List):
         return self.label
 
 
@@ -39,8 +39,8 @@ class Branch:
         self.split_value = split_value
         self.split_rows = split_rows
 
-    def predict(self, data: typing.Iterable):
-        if isinstance(self.value, int):
+    def predict(self, data: typing.List):
+        if isinstance(self.split_value, int):
             if data[self.split_col] == self.split_value:
                 return self.left.predict(data)
             else:
@@ -52,7 +52,42 @@ class Branch:
                 return self.right.predict(data)
 
 
+class Tree:
+    def __init__(self, max_depth: int, min_samples_split: int):
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
 
+    def fit(self, data: typing.List[typing.List]):
+        return self
+
+    def _find_best_split_on_feature(self, column_data: typing.List):
+        # finds best split on a feature/column
+        if isinstance(column_data[0], int):
+            # split as categorical
+            pass
+        if isinstance(column_data[0], float):
+            # split as continuous
+            pass
+
+    def _split_categorical_feature(self, column_data: typing.List):
+        possible_vals = set(column_data)
+        for possible_val in possible_vals:
+            left_split, right_split = [], []
+            for pt in column_data:
+                if pt == possible_val:
+                    left_split.append(pt)
+                else:
+                    right_split.append(pt)
+            # check both splits have at least min_samples_split
+            if (len(left_split) < self.min_samples_split or
+                        len(right_split) < self.min_samples_split):
+                continue
+            else:
+                
+        return NotImplemented
+
+    def predict(self):
+        return self
 
 def information_gain(parent_labels: typing.Iterable,
                      left_split_labels: typing.Iterable,
